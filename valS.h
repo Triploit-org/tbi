ff89a75d5df857700a02983f0714384b58aee09d
triploit <tristanpieper080803@gmail.com>
2016-08-11 17:26:16 +0200

    Update valS.h

--- valS.h
+++ valS.h
@@ -8,24 +8,24 @@
 // MfG, Survari
 //
 
-#define RAC 89
-#define SBC 88
+#define RAC 199
+#define SBC 216
 #define DFC 172
 #define HKC 173
 #define KLC 175
 
-int RA;           //  89 OR 01011001 || REG
-int SB;           //  88 OR 01011000 || REG
+int RA;           // 199 OR 11000111 || REG
+int SB;           // 216 OR 11011000 || REG
 int DF;           // 172 OR 10101100 || REG
 int HK;           // 173 OR 10101101 || REG
 int KL;           // 175 OR 10101111 || REG
 
-#define NREG 78   //  78 OR 01001110 || OUTPUT REG
+#define NREG 78   // 206 OR 11001110 || OUTPUT REG
 #define SY 17     //  17 OR 00010001 || SystemCall
 #define MV 24     //  24 OR 00011000 || MOV
 #define OUT 180   // 180 OR 10110100 || OUTPUT STREAM
 
-#define DEBG 81   //  81 OR 01010001 || DEBUG MODE
+#define DEBG 81   // 209 OR 11010001 || DEBUG MODE
 #define NL "@"    //   @ OR        @ || NEW LINE
 
 #endif
